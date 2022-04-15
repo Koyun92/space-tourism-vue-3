a9c5af55ff78478f0c12cdbc8151d250a5c4ef0a
Borys <borys.baca@gmail.com>
2022-04-15 18:48:16 +0200

    layout swiper correct

--- src/components/subpage.vue
+++ src/components/subpage.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="subpage__img">
-    <img
-      :src="require(`../assets${$route.path}/image-${data.name}.png`)"
-      :alt="data.name"
-    />
-  </div>
-  <div class="subpage__title">{{ data.name }}</div>
-  <div class="subpage__text">
-    <p>{{ data.description }}</p>
+  <div class="subpage">
+    <div class="subpage__img"></div>
+
+    <div class="subpage__title">{{ data.name }}</div>
+    <div class="subpage__text">
+      <p>{{ data.description }}</p>
+    </div>
+    <div class="swiper-pagination"></div>
   </div>
 </template>
 <script>
--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="subpage">
+  <div class="destination">
+    <img
+      :src="
+        require(`../assets${$route.path}/image-${
+          data[this.currentIndex].name
+        }.png`)
+      "
+      :alt="data.name"
+    />
+
     <swiper
+      ref="mySwiper"
+      :pagination="pagination"
+      :modules="modules"
+      class="mySwiper"
       :slides-per-view="1"
       :space-between="50"
       @swiper="onSwiper"
-      @slideChange="onSlideChange"
+      @slideChange="onSlide"
     >
       <swiper-slide v-for="data in data" :key="data.name">
-        <subpage :data="data" />
+        <subpage :pagination="pagination" :data="data" />
       </swiper-slide>
     </swiper>
   </div>
@@ -17,13 +30,12 @@ import data from "../assets/data.json";
 import { Swiper, SwiperSlide } from "swiper/vue/swiper-vue";
 import subpage from "../components/subpage.vue";
 import "swiper/swiper-bundle.css";
+import "swiper/modules/pagination/pagination";
+import "swiper/modules/pagination/pagination.min.css";
+import { Pagination } from "swiper";
+
 export default {
   name: "Destination",
-  data() {
-    return {
-      data: data.destinations,
-    };
-  },
 
   beforeCreate: function () {
     document.body.className = "destination-body";
@@ -36,20 +48,57 @@ export default {
     SwiperSlide,
     subpage,
   },
-  setup() {
+  data() {
     const onSwiper = (swiper) => {
       console.log(swiper);
     };
-    const onSlideChange = () => {
-      console.log("slide change");
+    const onSlideChange = (swiper) => {
+      console.log(swiper.activeIndex);
     };
+
     return {
+      data: data.destinations,
+      currentIndex: 0,
       onSwiper,
       onSlideChange,
+      pagination: {
+        clickable: true,
+        renderBullet: function (index, className) {
+          const names = ["MOON", "MARS", "EUROPA", "TITAN"];
+          return '<span class="' + className + '">' + names[index] + "</span>";
+        },
+      },
+
+      modules: [Pagination],
     };
+  },
+  methods: {
+    onSlide(swiper) {
+      console.log(swiper.activeIndex);
+      this.currentIndex = swiper.activeIndex;
+    },
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="css">
+.swiper-pagination {
+  position: static;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 2rem;
+  width: 100%;
+}
+.swiper-pagination-bullet {
+  width: 100%;
+  height: 100%;
+  padding-bottom: 1rem;
+  background-color: transparent;
+}
+.swiper-pagination-bullet-active {
+  background-color: transparent;
+  border-bottom: 1px solid hsl(231, 77%, 90%);
+  border-radius: 0%;
+}
 </style>
